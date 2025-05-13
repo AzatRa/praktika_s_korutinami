991f733dc2a340fcaecfc78804f488f135245d31
Азат Ра <azat.ra.xim@mail.ru>
2025-05-13 14:09:57 +0500

    Update Health.cs

--- Assets/Health.cs
+++ Assets/Health.cs
@@ -16,11 +16,13 @@ public class Health : MonoBehaviour
     private void OnEnable()
     {
         _button.onClick.AddListener(OnButtonClicked);
+        Debug.Log("AddListener(OnButtonClicked).");
     }
 
     private void OnDisable()
     {
         _button.onClick.RemoveListener(OnButtonClicked);
+        Debug.Log("RemoveListener(OnButtonClicked).");
     }
 
     private void Start()
@@ -31,12 +33,15 @@ public class Health : MonoBehaviour
 
     public void TakeDamage(float damage)
     {
+        Debug.Log("TakeDamage called");
         _currentHealth = Mathf.Clamp(_currentHealth - damage, 0, _maxHealth);
         Changed?.Invoke(_currentHealth);
+        Debug.Log("TakeDamage in Health.");
     }
 
     private void OnButtonClicked()
     {
-        TakeDamage(10f);
+        TakeDamage(1f);
+        Debug.Log("Button clicked");
     }
 }
